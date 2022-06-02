8b48eb7d0bc07f1ff4c298b9bd7b361eb92672e4
Amit Karpe <amitkarpe@gmail.com>
2022-06-02 11:49:15 +0800

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/bash
 
-sudo amazon-linux-extras install epel -y; 
+set -e
 
+tools {
+sudo amazon-linux-extras install epel -y; 
+}
+
+dev {
 # https://github.com/nvm-sh/nvm/#installing-and-updating
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 14.7.0
+}
 
+devops {
+sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && sudo yum -y install terraform
+terraform -install-autocomplete
+
+}
+
+main () {
+
+}
+
+main
