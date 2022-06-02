bc41f36461c14b5259e9c02624b5be4163b976e8
Amit Karpe <amitkarpe@gmail.com>
2022-06-02 11:44:02 +0800

    Create amazon.sh

---
+++ amazon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+sudo amazon-linux-extras install epel -y; 
+
+# https://github.com/nvm-sh/nvm/#installing-and-updating
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
