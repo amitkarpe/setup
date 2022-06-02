177e39e7bad8cd4787110d914fd2eeb26f171e48
Amit Karpe <amitkarpe@gmail.com>
2022-06-02 12:29:35 +0800

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -18,14 +18,21 @@ nvm install 14.7.0
 devops () {
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmpawscliv2.zip"
-unzip awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip /tmp/awscliv2.zip
 sudo ./aws/install
 aws --version
 
 sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && sudo yum -y install terraform
 terraform -install-autocomplete
+terraform  --version
+
+#curl https://github.com/gruntwork-io/terragrunt/releases/download/v0.37.1/terragrunt_linux_amd64 -o terragrunt
+wget -O terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v0.37.1/terragrunt_linux_amd64
+sudo install -o root -g root -m 0755 terragrunt /usr/local/bin/terragrunt
+terragrunt --version
 
 #brew install helm kubernetes-cli k9s octant kube-ps1 kubectx stern
 #brew install tektoncd-cli argocd
