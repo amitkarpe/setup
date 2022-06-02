db3fa0384d3b21d033b92b74ab17ebcbe69eac65
Amit Karpe <amitkarpe@gmail.com>
2022-06-02 15:28:37 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,14 +6,15 @@ install () {
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
-
+p10k configure
 }
 
 main () {
-
+  install
+  
 }
 
 main
