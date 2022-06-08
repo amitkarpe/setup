b4f992d814733f953877fe38065676cf155a5cf7
Amit Karpe <amitkarpe@gmail.com>
2022-06-08 18:13:49 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -7,12 +7,12 @@ sudo usermod $USER -s /usr/bin/zsh
 #zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-source ~/.zshrc
+#source ~/.zshrc
 
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-# echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
-# https://github.com/romkatv/powerlevel10k
-# p10k configure
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+https://github.com/romkatv/powerlevel10k
+p10k configure
 
 curl -o ~/.zshrc https://raw.githubusercontent.com/amitkarpe/setup/main/.zshrc
 curl -o ~/.oh-my-zsh/zsh-linux.sh https://raw.githubusercontent.com/amitkarpe/setup/main/zsh-linux.sh
@@ -20,6 +20,7 @@ curl -o ~/.oh-my-zsh/zsh-linux.sh https://raw.githubusercontent.com/amitkarpe/se
 cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting
 cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions
 
+echo "Run: source ~/.zshrc"
 
 }
 
