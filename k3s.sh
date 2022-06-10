2c9d0a58bdb813472ac322caaaceed9f8746ba6f
Amit Karpe <amitkarpe@gmail.com>
2022-06-10 14:55:42 +0530

    Update k3s.sh

--- k3s.sh
+++ k3s.sh
@@ -1,5 +1,8 @@
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker --no-deploy traefik
 
+# curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker 
+
+
 mkdir -p ~/.kube
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 export KUBECONFIG=~/.kube/config
