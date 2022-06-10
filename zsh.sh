92a7a1ae6f7d72e7d089637079dd200f5bb6b51a
Amit Karpe <amitkarpe@gmail.com>
2022-06-10 14:09:23 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,16 +3,24 @@
 set -e
 
 install () {
+echo "Installation started"
+
 sudo usermod $USER -s /usr/bin/zsh
 #zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+exit 0;
+}
 
+setup () {
+
+echo "Setup started"
 #source ~/.zshrc
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
-https://github.com/romkatv/powerlevel10k
-p10k configure
+# https://github.com/romkatv/powerlevel10k
+curl -o ~/.p10k.zsh https://raw.githubusercontent.com/amitkarpe/setup/main/.p10k.zsh
+# p10k configure
 
 #curl -o ~/.zshrc https://raw.githubusercontent.com/amitkarpe/setup/main/.zshrc
 #curl -o ~/.oh-my-zsh/zsh-linux.sh https://raw.githubusercontent.com/amitkarpe/setup/main/zsh-linux.sh
@@ -31,6 +39,7 @@ rm -v .zsh_history .zcompdump* .zshrc
 
 main () {
   install
+  setup
   
 }
 
