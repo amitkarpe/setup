394aec421bcde345834af3b0a9693cec59954364
Amit Karpe <amitkarpe@gmail.com>
2022-06-10 14:54:24 +0530

    Create k3s.sh

---
+++ k3s.sh
@@ -0,0 +1,10 @@
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker --no-deploy traefik
+
+mkdir -p ~/.kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+export KUBECONFIG=~/.kube/config
+kubectl get pods --all-namespaces
+
+echo $KUBECONFIG
+kubectl get node
+kubectl config view
