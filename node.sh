28820cb0138bc08f56a41331dd5e2302e3a640e4
Amit Karpe <amitkarpe@gmail.com>
2022-07-20 11:19:18 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -6,3 +6,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install 14.7.0
 node --version
 
+source ~/.zshrc
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+source ${HOME}/.profile
+
+npm install -g aws-azure-login
