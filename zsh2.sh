f14b25e45348bab8a2a16fca717d356303a6b89e
Amit Karpe <amitkarpe@gmail.com>
2022-07-20 09:55:11 +0530

    Update zsh2.sh

--- zsh2.sh
+++ zsh2.sh
@@ -18,15 +18,15 @@ setup () {
 echo "Setup started"
 #source ~/.zshrc
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 # https://github.com/romkatv/powerlevel10k
-curl -o ~/.p10k.zsh https://raw.githubusercontent.com/amitkarpe/setup/main/.p10k.zsh
+#curl -o ~/.p10k.zsh https://raw.githubusercontent.com/amitkarpe/setup/main/.p10k.zsh
 # p10k configure
 
 #mv ~/.zshrc ~/.zshrc.org
-cp ~/.zshrc ~/.zshrc.org
-curl -o ~/.zshrc https://raw.githubusercontent.com/amitkarpe/setup/main/.zshrc
+#cp ~/.zshrc ~/.zshrc.org
+#curl -o ~/.zshrc https://raw.githubusercontent.com/amitkarpe/setup/main/.zshrc
 #curl -o ~/.zshrc_extra https://raw.githubusercontent.com/amitkarpe/setup/main/.zshrc_extra
 #curl -o ~/.oh-my-zsh/zsh-linux.sh https://raw.githubusercontent.com/amitkarpe/setup/main/zsh-linux.sh
 
@@ -59,7 +59,7 @@ rm -v .zsh_history .zcompdump* .zshrc
 
 main () {
   install
-#  setup
+  setup
   
 }
 
