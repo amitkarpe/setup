e11a1a9531977482deb64af48b2ae96c6b3b787c
Amit Karpe <amitkarpe@gmail.com>
2022-08-24 15:43:21 +0530

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -7,3 +7,5 @@ export new=$1
 sudo useradd --create-home $new --groups wheel --shell /usr/bin/zsh --password 1
 #sudo usermod -s /usr/bin/zsh -a -G docker,wheel $new
 echo " $new ALL=(ALL)   NOPASSWD: ALL"  | sudo tee -a  /etc/sudoers
+
+echo "Run: chsh -s $(which zsh)"
