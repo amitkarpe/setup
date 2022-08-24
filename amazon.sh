e4866b5d257f4d694050bc19bcb237e440cb2552
Amit Karpe <amitkarpe@gmail.com>
2022-08-24 16:02:23 +0530

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -19,11 +19,40 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install 14.7.0
 
-sudo yum install golang -y
+#sudo yum install golang -y
+curl -OL https://go.dev/dl/go1.18.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.18.5.linux-amd64.tar.gz
+mkdir -p ~/go
+echo 'export GOPATH=$HOME/go' >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.profile
+source ~/.profile
+go version
 
 }
 
+docker () {
+#sudo apt-get remove -y docker docker-engine docker.io containerd runc
+#sudo mkdir -p /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update -y
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+sudo sh /tmp/get-docker.sh
+sudo usermod -a -G docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl enable docker
+sudo service docker start
+sudo systemctl status docker
+docker run hello-world
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+}
 
 git () {
 git config --global user.name "Amit Karpe"
@@ -35,6 +64,7 @@ git config --global credential.username amitkarpe
 main () {
   basic
   dev
+  docker
   git
 }
 
