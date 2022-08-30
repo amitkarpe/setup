30350a510a21832ea20ca3ffa67a9bb021595ddc
Amit Karpe <amitkarpe@gmail.com>
2022-08-30 20:43:33 +0530

    Update k3s.sh

--- k3s.sh
+++ k3s.sh
@@ -1,20 +1,17 @@
 # https://k3s.io/
 # https://github.com/k3s-io/k3s
 
-sudo systemctl enable docker
-sudo service docker start
+#sudo systemctl enable docker
+#sudo service docker start
 
-export INSTALL_K3S_VERSION="v1.21.5+k3s2"
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker
+#curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker
 
 # curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 777 --docker --no-deploy traefik
 
-
+export INSTALL_K3S_CHANNEL='stable'
+export INSTALL_K3S_VERSION="v1.23.10+k3s1"
+curl -sfL https://get.k3s.io | sh -
+sudo chmod +r /etc/rancher/k3s/k3s.yaml
 mkdir -p ~/.kube
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+cp -v /etc/rancher/k3s/k3s.yaml ~/.kube/config
 export KUBECONFIG=~/.kube/config
-kubectl get pods --all-namespaces
-
-echo $KUBECONFIG
-kubectl get node
-kubectl config view
