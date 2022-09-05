36ce40272d004d2a7c0f4986630d3b9eee85193f
Amit Karpe <amitkarpe@gmail.com>
2022-09-05 09:27:53 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,35 +4,41 @@ set -e
 
 basic () {
 sudo apt-get update -y # && sudo apt-get upgrade -y
-
-#sudo yum install -y curl tree tmux nano unzip vim wget git net-tools bash-completion zsh zsh-completion bind-utils bridge-utils jq
 # sudo apt  install -y tree tmux nano unzip vim wget git net-tools bind9-utils bridge-utils bash-completion zsh zsh-completion htop jq
 sudo apt-get install -y tree tmux nano unzip vim wget git net-tools zsh htop jq ca-certificates curl gnupg lsb-release
 #sudo apt install -y apt-file tasksel
-# bind9-utils bridge-utils bash-completion
+
 }
 
 dev () {
+if [[ ! -f $(which nvm) ]]
+then
 # https://github.com/nvm-sh/nvm/#installing-and-updating
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-source ~/.bashrc
-nvm install 14.7.0
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+if [[ ! -f $(which node) ]]
+then
+  nvm install 14.7.0
+fi
+nvm version
+node -v
+npm version
 
-# sudo apt install golang -y
-#curl -O https://storage.googleapis.com/golang/go1.13.5.linux-amd64.tar.gz
-#sudo rm -rf /usr/local/go
-#curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
-curl -OL https://go.dev/dl/go1.18.5.linux-amd64.tar.gz -s
-sudo tar -C /usr/local -xf go1.18.5.linux-amd64.tar.gz
-mkdir -p ~/go
-export GOPATH=$HOME/go
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.bashrc
-source ~/.bashrc
+
+if [[ ! -f $(which go) ]]
+then
+  curl -OL https://go.dev/dl/go1.18.5.linux-amd64.tar.gz -s
+  sudo tar -C /usr/local -xf go1.18.5.linux-amd64.tar.gz
+  mkdir -p ~/go
+  export GOPATH=$HOME/go
+  export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+  echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.bashrc
+  source ~/.bashrc
+fi
 go version
 }
 
@@ -77,6 +83,7 @@ git config --global core.excludesfile ~/.gitignore_global
 curl -o ~/.gitignore_global https://raw.githubusercontent.com/amitkarpe/setup/main/.gitignore_global
 
 export PAGER=''
+sleep 2
 git config --global --list
 cat ~/.gitconfig
 fi
@@ -84,6 +91,7 @@ fi
 
 
 main () {
+  sleep 2
   basic
   dev
   docker
