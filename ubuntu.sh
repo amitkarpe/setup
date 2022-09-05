a4d362d98b523c4b1d7de5d962d3d4b8c6377392
Amit Karpe <amitkarpe@gmail.com>
2022-09-05 08:04:37 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -75,6 +75,7 @@ git config --global color.status auto
 git config --global color.branch auto
 git config --global color.diff auto
 git config --global core.excludesfile ~/.gitignore_global
+curl -o ~/.gitignore_global https://raw.githubusercontent.com/amitkarpe/setup/main/.gitignore_global
 
 export PAGER=''
 git config --global --list
